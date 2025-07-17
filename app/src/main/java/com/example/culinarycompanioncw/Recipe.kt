1ded29560c4c8c458b3f0ed69e60c5fb0c7bdf2e
Justin Owusu <justinowusu722@gmail.com>
2025-07-17 18:21:18 +0100

    Recipe.kt class

---
+++ app/src/main/java/com/example/culinarycompanioncw/Recipe.kt
@@ -0,0 +1,13 @@
+package com.example.culinarycompanioncw
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+data class Recipe(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val ingredients: String,
+    val instructions: String,
+    val category: String  // Breakfast, Brunch, Lunch, etc.
+)
