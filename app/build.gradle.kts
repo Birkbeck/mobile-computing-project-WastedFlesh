692321afc0d5edb5fe32c2bc97be394ee7cdb3d5
Justin Owusu <justinowusu722@gmail.com>
2025-07-20 12:42:26 +0100

    Error fixes in the gradle builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
     id("kotlin.kapt")
+    id("com.google.devtools.ksp")
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
